9d892a937256977f1f0e7c6cb4325ee251604c92
Shuxin Huang <sh4409@columbia.edu>
2025-09-09 10:19:16 -0400

    Implemented features of posts
    New features including Create Post, Publish Post, List Published, Get Post Detail, Update Post, and JWT-based auth.

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { Sequelize } from 'sequelize';
+
+export const sequelize = new Sequelize(
+    process.env.DATABASE_NAME,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+        host: process.env.DATABASE_HOST,
+        dialect: 'mysql',
+        logging: false
+    }
+);
