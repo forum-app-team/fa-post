577e0d9e6bc88b63ae98f408b7d31af5c664949c
Shuxin Huang <sh4409@columbia.edu>
2025-09-11 05:04:37 -0400

    Implemented edit & delete posts
    Includes delete, recover,  ban, unban, archive, unarchive posts

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,7 +1,23 @@
 import { Router } from 'express';
 import { requireAuth } from '../middleware/auth.js';
 import { requireVerified } from '../middleware/verified.js';
-import { createPost, publishPost, listPublished, getPost, updatePost } from '../controllers/posts.controller.js';
+import { requireOwner } from "../middleware/ownership.js";
+import { requireAdmin } from '../middleware/roles.js';
+import {
+    createPost,
+    publishPost,
+    listPublished,
+    getPost,
+    updatePost,
+    deletePost,
+    recoverPost,
+    archivePost,
+    unarchivePost,
+    hidePost,
+    unhidePost,
+    banPost,
+    unbanPost
+} from '../controllers/posts.controller.js';
 
 const r = Router();
 
@@ -11,5 +27,16 @@ r.get('/:id', requireAuth, getPost);
 r.post('/', requireAuth, requireVerified, createPost);
 r.put('/:id', requireAuth, requireVerified, updatePost);
 r.put('/:id/publish', requireAuth, requireVerified, publishPost);
+
+r.delete('/:id', requireAuth, requireVerified, deletePost);
+
+r.post('/:id/archive',   requireAuth, requireVerified, archivePost);
+r.post('/:id/unarchive', requireAuth, requireVerified, unarchivePost);
+r.post('/:id/hide',      requireAuth, requireVerified, hidePost);
+r.post('/:id/unhide',    requireAuth, requireVerified, unhidePost);
+
+r.post('/:id/ban',     requireAuth, requireVerified, requireAdmin, banPost);
+r.post('/:id/unban',   requireAuth, requireVerified, requireAdmin, unbanPost);
+r.post('/:id/recover', requireAuth, requireVerified, requireAdmin, recoverPost);
 
 export default r;
