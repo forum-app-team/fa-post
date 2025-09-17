20e535c5c858e200bc6380af17050dd395fcc2ee
ChanradyHo <cradyh.wk@gmail.com>
2025-09-17 09:06:21 -0700

    feat/SCRUM-42: added 'top' and 'drafts' routes for frontend integration

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -27,6 +27,14 @@ import {
 
 const r = Router();
 
+
+import { listTopPosts, listDrafts } from '../controllers/posts.controller.js';
+
+// Top posts (by replies, desc)
+r.get('/top', requireAuth, listTopPosts);
+// Drafts (unpublished posts for current user)
+r.get('/drafts', requireAuth, listDrafts);
+
 // Posts
 r.get('/', requireAuth, listPublished);
 r.get('/:id', requireAuth, getPost);
